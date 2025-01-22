8d0c75f3c516cf90662c88241d6dce4b269e6e5a
hurtcopain <jacksummers715@gmail.com>
2025-01-22 00:58:38 -0500

    import from Parasite

---
+++ config.mk
@@ -0,0 +1,5 @@
+# Inherit from telephony config
+$(call inherit-product, vendor/google/pixel-additional/products/telephony.mk)
+
+# Inherit from gms config
+$(call inherit-product, vendor/google/pixel-additional/products/gms.mk)
